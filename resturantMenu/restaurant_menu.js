c120a6a509bf871ce950ae6395b46e662056477e
fyavuz96 <fyavuz17@posta.pau.edu.tr>
2025-04-06 15:02:09 +0300

    Resturant Menu Created

---
+++ resturantMenu/restaurant_menu.js
@@ -0,0 +1,16 @@
+const breakfastMenu = ['Pancakes', 'Eggs Benedict', 'Oatmeal', 'Frittata'];
+const mainCourseMenu = ['Steak', 'Pasta', 'Burger', 'Salmon'];
+const dessertMenu = ['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad'];
+
+const breakfastMenuItemsHTML = breakfastMenu.map((item, index) => `<p>Öğe ${index + 1}: ${item}</p>`).join('');
+document.getElementById('breakfastMenuItems').innerHTML = breakfastMenuItemsHTML;
+
+let mainCourseItem = '';
+mainCourseMenu.forEach((item, index) => {
+mainCourseItem += `<p>Öğe ${index + 1}: ${item}</p>`;});
+document.getElementById('maincourseMenuItems').innerHTML = mainCourseItem;
+
+let dessertItem = '';
+for (let i = 0; i < dessertMenu.length; i++) {
+    dessertItem += `<p>Öğe ${i + 1}: ${dessertMenu[i]}</p>`;}
+document.getElementById('dessertMenuItems').innerHTML = dessertItem;
